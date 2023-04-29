a0f11f7ae2674d6ad24aa5882171475238417c49
Matthew Spire <matthew.t.spire@gmail.com>
2023-04-29 10:08:14 -0400

    Initial Commit

---
+++ CharacterViewer/CharacterViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterViewerApp.swift
+//  CharacterViewer
+//
+//  Created by Matthew Spire on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct CharacterViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
